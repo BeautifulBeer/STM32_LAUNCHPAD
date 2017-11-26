f645bb679159d67e6713ae15587e2e69ed9a9bdc
BeautifulBeer <dbsrhksdnd@gmail.com>
2017-11-26 17:12:56 +0900

    Bluetooth update, Setting clock is added

---
+++ Project/launchpad/Debug/Libraries/subdir.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Libraries/Touch.c \
+../Libraries/lcd.c 
+
+OBJS += \
+./Libraries/Touch.o \
+./Libraries/lcd.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Libraries/%.o: ../Libraries/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM C Compiler 5'
+	armcc -I"C:\Users\hw_5\Desktop\git_repository\Project\launchpad\Libraries" -I"C:\Users\hw_5\Desktop\git_repository\Project\launchpad\Libraries\CMSIS\CoreSupport" -I"C:\Users\hw_5\Desktop\git_repository\Project\launchpad\Libraries\CMSIS\DeviceSupport" -I"C:\Users\hw_5\Desktop\git_repository\Project\launchpad\Libraries\CMSIS\DeviceSupport\Startup" -I"C:\Users\hw_5\Desktop\git_repository\Project\launchpad\Libraries\STM32F10x_StdPeriph_Driver_v3.5\inc" -I"C:\Users\hw_5\Desktop\git_repository\Project\launchpad\Libraries\STM32F10x_StdPeriph_Driver_v3.5\src" -O0 --cpu=cortex-m3 -g -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Project/launchpad/Debug/bluetooth/subdir.mk
@@ -0,0 +1,21 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../bluetooth/config_bluetooth.c 
+
+OBJS += \
+./bluetooth/config_bluetooth.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+bluetooth/%.o: ../bluetooth/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM C Compiler 5'
+	armcc -I"C:\Users\hw_5\Desktop\git_repository\Project\launchpad\Libraries" -I"C:\Users\hw_5\Desktop\git_repository\Project\launchpad\Libraries\CMSIS\CoreSupport" -I"C:\Users\hw_5\Desktop\git_repository\Project\launchpad\Libraries\CMSIS\DeviceSupport" -I"C:\Users\hw_5\Desktop\git_repository\Project\launchpad\Libraries\CMSIS\DeviceSupport\Startup" -I"C:\Users\hw_5\Desktop\git_repository\Project\launchpad\Libraries\STM32F10x_StdPeriph_Driver_v3.5\inc" -I"C:\Users\hw_5\Desktop\git_repository\Project\launchpad\Libraries\STM32F10x_StdPeriph_Driver_v3.5\src" -O0 --cpu=cortex-m3 -g -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
